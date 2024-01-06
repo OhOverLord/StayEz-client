5a8338a8455dc8fd70d5dcf9f1087740c21f73e6
Leonid Filippov <filipleo@fit.cvut.cz>
2024-01-06 01:50:40 +0100

    Add 404 not found page

---
+++ src/components/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container text-center mt-5">
+      <h1>404</h1>
+      <p>Page Not Found</p>
+      <img src="@/assets/corgi_not_found.png" alt="404" class="img-fluid">
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NotFound'
+  }
+  </script>
+  
+  <style>
+  </style>
+  
--- src/router.js
+++ src/router.js
@@ -3,12 +3,14 @@ import Hotels from './components/Hotels.vue'
 import HotelDetails from './components/HotelDetails.vue'
 import RoomReservation from './components/RoomReservation.vue'
 import ReservationDetails from './components/ReservationDetails.vue'
+import NotFound from './components/NotFound.vue'
 
 const routes = [
   { path: '/', component: Hotels },
   { path: '/hotel/:id', component: HotelDetails, props: true },
   { path: '/reserve/:id', component: RoomReservation, props: true },
-  { path: '/reservation-details/:id', component: ReservationDetails, props: true }
+  { path: '/reservation-details/:id', component: ReservationDetails, props: true },
+  { path: '/:catchAll(.*)', component: NotFound }
 ]
 
 const router = createRouter({
